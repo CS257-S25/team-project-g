3c89f72832cc5a4f5d469276ea3fc8be7f470b16
Joseph Borncamp <jborncamp@gmail.com>
2025-04-21 19:29:43 -0500

    Blueprint for tets.

---
+++ Tests/tests.py
@@ -0,0 +1,60 @@
+from .. import cl
+import unittest
+import sys
+
+if __name__ == "__main__":
+    unittest.main()
+
+class Test_command_line(unittest.TestCase):
+    '''
+    This class tests the command line interface of the cl.py.
+    '''
+    def test_get_title(self):
+        '''
+        Arguments: None
+        Return value: Return true if the test passes.
+        This test grabs the title of a book.
+        '''
+    
+    def test_get_author(self):
+        '''
+        Arguments: None
+        Return value: Return true if the test passes.
+        This test grabs the author of a book.
+        '''
+
+    def test_get_genre(self):
+        '''
+        Arguments: None
+        Return value: Return true if the test passes.
+        This test grabs the genre of a book.
+        '''
+
+    def test_get_most_banned_districts(self):
+        '''
+        Arguments: None
+        Return value: Return true if the test passes.
+        This test grabs the most banned districts.
+        '''
+
+    def test_get_most_banned_authors(self):
+        '''
+        Arguments: None
+        Return value: Return true if the test passes.
+        This test grabs the most banned authors.
+        '''
+    
+    def test_get_most_banned_states(self):
+        '''
+        Arguments: None
+        Return value: Return true if the test passes.
+        This test grabs the most banned states.
+        '''
+
+    def test_get_most_banned_titles(self):
+        '''
+        Arguments: None
+        Return value: Return true if the test passes.
+        This test grabs the most banned titles.
+        '''
+#These are blueprints for tests.

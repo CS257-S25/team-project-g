a8716c09b13f4882f2897f7e3e11c2fa6bf6e54c
kohkohmonster <jborncamp@gmail.com>
2025-04-21 21:52:17 -0500

    Update and rename tests.py to test_cl.py

--- Tests/test_cl.py
+++ Tests/test_cl.py
@@ -1,4 +1,6 @@
 from .. import cl
+from ProductionCode import search
+from ProductionCode import most_banned
 import unittest
 import sys
 
@@ -9,12 +11,20 @@ class Test_command_line(unittest.TestCase):
     '''
     This class tests the command line interface of the cl.py.
     '''
-    def test_get_title(self):
+    def test_cl_app(self):
         '''
         Arguments: None
         Return value: Return true if the test passes.
         This test grabs the title of a book.
         '''
+
+        sys.argv=['cl.py', '--search-title', 'Cool for the Summer']
+        cl.main()
+        self.assertEqual(cl.get_title(), 'Cool for the Summer')
+        
+        sys.argv=['cl.py', '--help']
+        cl.main()
+        self.assertIn(cl.get_help(), sys.stdout.get_help())
     
     def test_get_author(self):
         '''
@@ -37,12 +47,24 @@ class Test_command_line(unittest.TestCase):
         This test grabs the most banned districts.
         '''
 
+        # Simulate normal test.
+        self.assertEqual(most_banned.most_banned_districts(5), 'Fake District: 5')
+
+        # Simulate edge case test.
+        self.assertEqual(most_banned.most_banned_districts(-5), 'Fake District: 0')
+
     def test_get_most_banned_authors(self):
         '''
         Arguments: None
         Return value: Return true if the test passes.
         This test grabs the most banned authors.
         '''
+
+        # Simulate normal test.
+        self.assertEqual(most_banned.most_banned_authors(3), 'George Orwell: 1')
+
+        # Simulate edge case test.
+        self.assertEqual(most_banned.most_banned_authors(-3), 'Fake Author: 0')
     
     def test_get_most_banned_states(self):
         '''
@@ -51,10 +73,30 @@ class Test_command_line(unittest.TestCase):
         This test grabs the most banned states.
         '''
 
+        # Simulate normal test.
+        self.assertEqual(most_banned.most_banned_states(1), 'Texas: 1')
+
+        # Simulate edge case test.
+        self.assertEqual(most_banned.most_banned_states(-1), 'Fake State: 0')
+
     def test_get_most_banned_titles(self):
         '''
         Arguments: None
         Return value: Return true if the test passes.
         This test grabs the most banned titles.
         '''
+
+        # Simulate normal test.
+        self.assertEqual(most_banned.most_banned_titles(1), 'The Hate U Give: 1')
+
+        # Simulate edge case test.
+        self.assertEqual(most_banned.most_banned_titles(0), 'Fake Title: 0')
+    
+    def test_get_help(self):
+        '''
+        Arguments: None
+        Return value: Return true if the test passes.
+        This test grabs the help message.
+        '''
+    
 #These are blueprints for tests.

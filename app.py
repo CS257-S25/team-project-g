0128ca15563914d069b88a43ec6da1930cd98969
Marco <pinam@carleton.edu>
2025-04-23 12:49:22 -0500

    added most-banned routes to app.py

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, abort
+from ProductionCode.most_banned import (
+    most_banned_districts,
+    most_banned_authors,
+    most_banned_states,
+    most_banned_titles)
 
 app = Flask(__name__)
 
+most_banned_map = {
+    "states": most_banned_states,
+    "districts": most_banned_districts,
+    "authors": most_banned_authors,
+    "titles": most_banned_titles
+}
 
 @app.route("/")
 def homepage():
@@ -18,11 +29,25 @@ def search(field, query):
     pass
 
 
-@app.route("/most-banned/<field>/<query>")
+@app.route('/most-banned/<field>/<limit>', strict_slashes=False)
 def most_banned(field, limit):
-    pass
+    '''
+    The endpoint for the most banned titles
+    '''
+    if not limit.isdigit() or field not in most_banned_map:
+        abort(400)
+        
+    function = most_banned_map[field]
+    return function(int(limit))
+    
 
+@app.errorhandler(400)
+def python_bug(e):
+    '''
+    The endpoint for the most banned titles
+    '''
+    return "400: Bad Request", 400
 
-if __name__ == "./main__":
+if __name__ == "__main__":
     app.run()
 

dfa2d659639dca10d7a7e3bdf666009ac7c20f32
Marco <pinam@carleton.edu>
2025-04-22 19:15:00 -0500

    Fixed too many branches style check

--- cl.py
+++ cl.py
@@ -87,39 +87,26 @@ def main():
         metavar="LIMIT",
     )
     args = parser.parse_args()
-    if args.search_title:
-        search_results = search_title(args.search_title)
-        for result in search_results:
-            print(result)
-    elif args.search_author:
-        search_results = search_author(args.search_author)
-        for result in search_results:
-            print(result)
-    elif args.search_genre:
-        search_results = search_genre(args.search_genre)
-        for result in search_results:
-            print(result)
-    elif args.most_banned_districts:
-        search_results = most_banned_districts(args.most_banned_districts)
-        for result in search_results:
-            print(result)
-    elif args.most_banned_authors:
-        search_results = most_banned_authors(args.most_banned_authors)
-        for result in search_results:
-            print(result)
-    elif args.most_banned_states:
-        search_results = most_banned_states(args.most_banned_states)
-        for result in search_results:
-            print(result)
-    elif args.most_banned_titles:
-        search_results = most_banned_titles(args.most_banned_titles)
-        for result in search_results:
-            print(result)
-    # If no arguments are provided, print the help message
+    
+    dispatch = {
+        "search_title": search_title,
+        "search_author": search_author,
+        "search_genre": search_genre,
+        "most_banned_districts": most_banned_districts,
+        "most_banned_authors": most_banned_authors,
+        "most_banned_states": most_banned_states,
+        "most_banned_titles": most_banned_titles,
+    }
+    
+    for attr, func in dispatch.items():
+        if getattr(args, attr) is not None:
+            search_results = func(getattr(args, attr))
+            for result in search_results:
+                print(result)
+            break
     else:
         parser.print_help()
         sys.exit(1)
-
 
 if __name__ == "__main__":
     main()

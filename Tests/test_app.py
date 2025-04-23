8a8d6bf29af7d550d2b400112272662653718c19
Marco <pinam@carleton.edu>
2025-04-23 13:34:23 -0500

    Fixed whitespace on test_app.py

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -7,7 +7,7 @@ class TestApp(unittest.TestCase):
 
     def test_homepage(self):
         pass
-    
+
     def test_valid_states_route(self):
         '''
         Arguments: None
@@ -17,7 +17,7 @@ class TestApp(unittest.TestCase):
         response = self.app.get('/most-banned/states/5')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Florida: 6533Iowa: 3685Texas: 1964Pennsylvania: 664Wisconsin: 480', response.data)
-        
+
     def test_valid_titles_route(self):
         '''
         Arguments: None
@@ -27,7 +27,7 @@ class TestApp(unittest.TestCase):
         response = self.app.get('/most-banned/titles/5')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Looking for Alaska: 135Nineteen Minutes: 126The Perks of Being a Wallflower: 118Sold: 116Thirteen Reasons Why: 112', response.data)
-        
+
     def test_valid_districts_route(self):
         '''
         Arguments: None
@@ -37,7 +37,7 @@ class TestApp(unittest.TestCase):
         response = self.app.get('/most-banned/districts/5')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Escambia County Public Schools: 1787Clay County School District: 864Orange County Public Schools: 734North East Independent School District: 606Central York School District: 443', response.data)
-        
+
     def test_valid_authors_route(self):
         '''
         Arguments: None
@@ -47,7 +47,7 @@ class TestApp(unittest.TestCase):
         response = self.app.get('/most-banned/authors/5')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Ellen Hopkins: 791Sarah J. Maas: 657Jodi Picoult: 213John Green: 203Toni Morrison: 197', response.data)
-        
+
     def test_invalid_limit(self):
         '''
         Arguments: None

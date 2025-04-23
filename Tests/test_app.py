3b5cc3da98674b9855fa65d29c5a24320b366e92
Marco <pinam@carleton.edu>
2025-04-23 13:39:54 -0500

    added docstrings and made lines shorter for test_app.py

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -1,11 +1,27 @@
+'''
+This file contains the unit tests for the Flask application.
+'''
 import unittest
 from app import app
 
 class TestApp(unittest.TestCase):
+    '''
+    This class tests the Flask application.
+    '''
     def setUp(self):
+        '''
+        Arguments: None
+        Return value: None
+        This function sets up the test client for the Flask application.
+        '''
         self.app = app.test_client()
 
     def test_homepage(self):
+        '''
+        Arguments: None
+        Return value: None
+        This function tests the homepage of the Flask application.
+        '''
         pass
 
     def test_valid_states_route(self):
@@ -16,7 +32,8 @@ class TestApp(unittest.TestCase):
         '''
         response = self.app.get('/most-banned/states/5')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Florida: 6533Iowa: 3685Texas: 1964Pennsylvania: 664Wisconsin: 480', response.data)
+        self.assertIn(b'Florida: 6533Iowa: 3685Texas: 1964Pennsylvania: 664Wisconsin: 480',
+                      response.data)
 
     def test_valid_titles_route(self):
         '''
@@ -26,7 +43,9 @@ class TestApp(unittest.TestCase):
         '''
         response = self.app.get('/most-banned/titles/5')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Looking for Alaska: 135Nineteen Minutes: 126The Perks of Being a Wallflower: 118Sold: 116Thirteen Reasons Why: 112', response.data)
+        self.assertIn(b'Looking for Alaska: 135Nineteen Minutes: 126The Perks of Being a Wallflower: 118'
+                      b'Sold: 116Thirteen Reasons Why: 112',
+                      response.data)
 
     def test_valid_districts_route(self):
         '''
@@ -36,17 +55,21 @@ class TestApp(unittest.TestCase):
         '''
         response = self.app.get('/most-banned/districts/5')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Escambia County Public Schools: 1787Clay County School District: 864Orange County Public Schools: 734North East Independent School District: 606Central York School District: 443', response.data)
+        self.assertIn(b'Escambia County Public Schools: 1787Clay County School District: 864'
+                      b'Orange County Public Schools: 734North East Independent School District: 606'
+                      b'Central York School District: 443',
+                      response.data)
 
     def test_valid_authors_route(self):
         '''
         Arguments: None
         Return value: Return true if the test passes.
         This test grabs the most banned authors.
-        ''' 
+        '''
         response = self.app.get('/most-banned/authors/5')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Ellen Hopkins: 791Sarah J. Maas: 657Jodi Picoult: 213John Green: 203Toni Morrison: 197', response.data)
+        self.assertIn(b'Ellen Hopkins: 791Sarah J. Maas: 657Jodi Picoult: 213John Green: 203Toni Morrison: 197',
+                      response.data)
 
     def test_invalid_limit(self):
         '''

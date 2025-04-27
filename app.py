4b65780d66961811dcd272e0088db68904f7c93a
cteone <108945107+cteone@users.noreply.github.com>
2025-04-27 15:34:20 -0500

    Updated app.py

--- app.py
+++ app.py
@@ -99,12 +99,15 @@ def format_list_with_linebreak(list_of_strings):
     """
     return "<br>".join(list_of_strings)
 
+
 @app.errorhandler(404)
 def page_not_found(_error):
-    return ("400: Sorry page not found"
-            "<br>Input correct route like this"
-            "<br>/search/OPTION/OPTION_NAME"
-            "<br>/most-banned/OPTION/MAX_INT")
+    return (
+        "400: Sorry page not found"
+        "<br>Input correct route like this"
+        "<br>/search/OPTION/OPTION_NAME"
+        "<br>/most-banned/OPTION/MAX_INT"
+    )
 
 
 if __name__ == "__main__":

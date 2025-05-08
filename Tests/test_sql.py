892695e45d2aef714aff9607ae4a224a0163f3e9
Marco <pinam@carleton.edu>
2025-05-07 20:02:45 -0500

    added tests but not fully implemented tests

--- Tests/test_sql.py
+++ Tests/test_sql.py
@@ -6,17 +6,15 @@ import unittest
 from unittest.mock import MagicMock, patch
 from ProductionCode.datasource import (
     books_search_title,
-    search_author,
-    search_title_like,
-    search_genre,
-    get_bans_per_year,
-    get_most_common_words,
+    book_from_isbn,
+    books_search_author,
+    books_search_genre,
     get_most_banned_authors,
-    get_keyword,
     get_most_banned_districts,
     get_most_banned_states,
     get_most_banned_titles
-)
+    )
+
 
 class TestSQLQueries(unittest.TestCase):
     """
@@ -40,9 +38,46 @@ class TestSQLQueries(unittest.TestCase):
         mock_connect.return_value = self.mock_conn
             #set what it should return
         self.mock_cursor.fetchone.return_value = (response)
-        self.assertEqual(books_search_title('killing'), response)
+        self.assertEqual(books_search_title('Killing Jesus: '), response)
+
+    def test_search_isbn(self, mock_connect):
+        response = ()
+        mock_connect.return_value = self.mock_conn
+        self.mock_cursor.fetchone.return_value = response
+        self.asseertEqual(book_from_isbn(440236924), response)
 
     def test_search_author(self, mock_connect):
-        response = (
-            
-        )
+        response = ()
+        mock_connect.return_value = self.mock_conn
+        self.mock_cursor.fetchone.return_value = response
+        self.asseertEqual(books_search_author('Kaleidoscope'), response)
+
+    def test_search_genre(self, mock_connect):
+        response = ()
+        mock_connect.return_value = self.mock_conn
+        self.mock_cursor.fetchone.return_value = response
+        self.assertEqual(books_search_genre('Animal Fiction'), response)
+
+    def test_get_most_banned_authors(self, mock_connect):
+        response = ()
+        mock_connect.return_value = self.mock_conn
+        self.mock_cursor.fetchone.return_value = response
+        self.assertEqual(get_most_banned_authors(1), response)
+
+    def test_get_most_banned_districts(self, mock_connect):
+        response = ()
+        mock_connect.return_value = self.mock_conn
+        self.mock_cursor.fetchone.return_value = response
+        self.assertEqual(get_most_banned_districts(1), response)
+
+    def test_get_most_banned_states(self, mock_connect):
+        response = ()
+        mock_connect.return_value = self.mock_conn
+        self.mock_cursor.fetchone.return_value = response
+        self.assertEqual(get_most_banned_states(1), response)
+
+    def test_get_most_banned_titles(self, mock_connect):
+        response = ()
+        mock_connect.return_value = self.mock_conn
+        self.mock_cursor.fetchone.return_value = response
+        self.assertEqual(get_most_banned_titles(1), response)

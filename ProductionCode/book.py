309f9c3ba4e5940d7f891cdbb6d17f3e98c06e86
Cooper <evansc2@carleton.edu>
2025-05-19 04:02:14 -0400

    fix genres to string in Book

--- ProductionCode/book.py
+++ ProductionCode/book.py
@@ -35,7 +35,10 @@ class Book:
 
     def genres_to_string(self) -> str:
         """Helper method for displaying author information"""
-        return ", ".join(self.details["genres"])
+        if self.details["genres"]:
+            return ", ".join(self.details["genres"])
+        else:
+            return ""
 
     # def average_rating_histogram(self, rating_histogram):
     #     return round(fmean([1, 2, 3, 4, 5], weights=rating_histogram), 1)

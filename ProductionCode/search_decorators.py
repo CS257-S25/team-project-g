16822947ade331f6a7b2692fd65184dae30ba3fa
cteone <108945107+cteone@users.noreply.github.com>
2025-05-31 12:23:10 -0500

    Added docstrings for search_decorators.py and search_strategies.py

--- ProductionCode/search_decorators.py
+++ ProductionCode/search_decorators.py
@@ -1,11 +1,24 @@
+"""Decorators for modifying search functionality"""
+
 from ProductionCode.book import Book
 from ProductionCode.datasource import DataSource
 
+
 class SearchSection:
-    def __init__(self, heading: str, type: str, results: list[Book]):
+    """Defines a section of the search results"""
+
+    def __init__(self, heading: str, search_type: str, results: list[Book]) -> None:
+        """
+        Constructor for SearchSection
+        Arguments:
+            heading (str) - heading of the section (displayed)
+            type (str) - type of the section (internal)
+            results (list[Book]) - list of books in the section
+        """
         self.heading = heading
-        self.type = type
+        self.search_type = search_type
         self.results = results
+
 
 class SearchComponent:
     """
@@ -13,7 +26,8 @@ class SearchComponent:
     decorators.
     """
 
-    def operation(self, query) -> list[SearchSection]:
+    def operation(self, _query) -> list[SearchSection]:
+        """Operation to be implimented by decorators"""
         return []
 
 
@@ -22,7 +36,8 @@ class SearchConcreteComponent(SearchComponent):
     SearchConcreteComponent provides default implimentation for SearchComponent interface
     """
 
-    def operation(self, query) -> list[SearchSection]:
+    def operation(self, _query) -> list[SearchSection]:
+        """Default operation implimentation"""
         return []
 
 
@@ -32,16 +47,30 @@ class SearchDecorator:
     """
 
     def __init__(self, search_component: SearchComponent) -> None:
+        """
+        Constructor for SearchDecorator
+        Arguments:
+            search_component (SearchComponent) - the wrapped component
+        """
         self._search_component: SearchComponent = search_component
 
     @property
     def search_component(self) -> SearchComponent:
         """
         The SearchDecorator delegates all work to the wrapped component
+        Returns:
+            (SearchComponent) - the wrapped component
         """
         return self._search_component
 
     def operation(self, query) -> list[SearchSection]:
+        """
+        The SearchDecorator calls the operation on the wrapped component
+        Arguments:
+            query (str) - the search query
+        Returns:
+            (list[SearchSection]) - search results divided into sections
+        """
         return self._search_component.operation(query)
 
 
@@ -51,14 +80,28 @@ class SearchConcreteDecoratorAuthor(SearchDecorator):
     """
 
     def operation(self, query) -> list[SearchSection]:
+        """
+        Author search
+        Arguments:
+            query (str) - the search query
+        Returns:
+            (list[SearchSection]) - search results divided into sections
+        """
         search_author = self.search_author(query)
         return self.search_component.operation(query) + [search_author]
 
     def search_author(self, query):
+        """
+        Author search database call
+        Arguments:
+            query (str) - the search query
+        Returns:
+            (SearchSection) - author search section
+        """
         ds = DataSource()
         results = ds.books_search_author(query)
         section = SearchSection("Author", "author", results)
-        return section 
+        return section
 
 
 class SearchConcreteDecoratorISBN(SearchDecorator):
@@ -67,16 +110,29 @@ class SearchConcreteDecoratorISBN(SearchDecorator):
     """
 
     def operation(self, query) -> list[SearchSection]:
+        """
+        ISBN search
+        Arguments:
+            query (str) - the search query
+        Returns:
+            (list[SearchSection]) - search results divided into sections
+        """
         search_isbn = self.search_isbn(query)
         return self.search_component.operation(query) + [search_isbn]
 
     def search_isbn(self, query):
+        """
+        ISBN search database call
+        Arguments:
+            query (str) - the search query
+        Returns:
+            (SearchSection) - isbn search section
+        """
         ds = DataSource()
         results = ds.book_from_isbn(query)
         if results:
             return SearchSection("ISBN", "isbn", [results])
-        else:
-            return SearchSection("ISBN", "isbn", [])
+        return SearchSection("ISBN", "isbn", [])
 
 
 class SearchConcreteDecoratorTitle(SearchDecorator):
@@ -85,23 +141,42 @@ class SearchConcreteDecoratorTitle(SearchDecorator):
     """
 
     def operation(self, query) -> list[SearchSection]:
+        """
+        Title search
+        Arguments:
+            query (str) - the search query
+        Returns:
+            (list[SearchSection]) - search results divided into sections
+        """
         search_isbn = self.search_title(query)
         return self.search_component.operation(query) + [search_isbn]
 
     def search_title(self, query):
+        """
+        Title search database call
+        Arguments:
+            query (str) - the search query
+        Returns:
+            (SearchSection) - title search section
+        """
         ds = DataSource()
         results = ds.books_search_title(query)
         section = SearchSection("Title", "title", results)
-        return section 
+        return section
 
 
+class SearchConcreteDecoratorLimitResults(SearchDecorator):
+    """
+    Limits the number of results in each section
+    """
 
-def search(search_component: SearchComponent, query) -> None:
+    max_results = 5
+    # eventually, max a parameter
+    # def __init__(self, max_results):
+    #     self.max_results = max_results
 
-   results = search_component.operation(query)
-   return results
-
-
-if __name__ == "__main__":
-    simple = SearchConcreteComponent()
-    search(simple)
+    def operation(self, query) -> list[SearchSection]:
+        results = self.search_component.operation(query)
+        for section in results:
+            section.results = section.results[: self.max_results]
+        return results
--- ProductionCode/search_strategies.py
+++ ProductionCode/search_strategies.py
@@ -1,36 +1,111 @@
+"""Module for Search Strategies"""
+
 from abc import ABC, abstractmethod
 from ProductionCode.search_decorators import SearchSection
-from ProductionCode.search_decorators import SearchConcreteComponent, SearchConcreteDecoratorAuthor, SearchConcreteDecoratorISBN, SearchConcreteDecoratorTitle
+from ProductionCode.search_decorators import (
+    SearchConcreteComponent,
+    SearchConcreteDecoratorAuthor,
+    SearchConcreteDecoratorISBN,
+    SearchConcreteDecoratorTitle,
+    SearchConcreteDecoratorLimitResults,
+)
 
-class SearchContext():
-    def __init__(self, search_strategy) -> None:
-        self._search_strategy = search_strategy
-    
-    def search(self, query) -> list[SearchSection]:
-        return self._search_strategy.search(query)
-    
+
 class SearchStrategy(ABC):
+    """Interface for defining search functionality of sections"""
+
     @abstractmethod
     def search(self, query) -> list[SearchSection]:
-        pass
+        """
+        Searches books database
+        Arguments:
+            query (str) - the search query
+        Returns:
+            (list[SearchSection]) - search results, divided into sections
+        """
+
+
+class SearchContext:
+    """
+    Defines the interface that is interacted with by the client
+    """
+
+    def __init__(self, search_strategy: SearchStrategy) -> None:
+        """
+        Constructor for SearchContext
+        Arguments:
+            search_strategy (SearchStrategy) - strategy used to search
+        """
+
+        self._search_strategy = search_strategy
+
+    def search(self, query: str) -> list[SearchSection]:
+        """
+        Searches books database
+        Arguments:
+            query (str) - the search query
+        Returns:
+            (list[SearchSection]) - search results, divided into sections
+        """
+        return self._search_strategy.search(query)
+
 
 class ConcreteSearchStrategyAll(SearchStrategy):
+    """
+    Strategy that searches for authors, titles, and isbn
+    """
+
     def search(self, query) -> list[SearchSection]:
-        MAX_RESULTS = 5
-        search_component = SearchConcreteDecoratorAuthor(SearchConcreteDecoratorTitle(SearchConcreteDecoratorISBN(SearchConcreteComponent())))
+        """
+        Searches books database
+        Arguments:
+            query (str) - the search query
+        Returns:
+            (list[SearchSection]) - search results, divided into sections
+        """
+        search_component = SearchConcreteDecoratorLimitResults(
+            SearchConcreteDecoratorAuthor(
+                SearchConcreteDecoratorTitle(
+                    SearchConcreteDecoratorISBN(SearchConcreteComponent())
+                )
+            )
+        )
         results = search_component.operation(query)
-        [setattr(result, 'results', result.results[:MAX_RESULTS]) for result in results]
+        # [setattr(result, "results", result.results[:MAX_RESULTS]) for result in results]
         return results
 
 
 class ConcreteSearchStrategyTitle(SearchStrategy):
+    """
+    Strategy that searches for titles
+    """
+
     def search(self, query) -> list[SearchSection]:
+        """
+        Searches books database
+        Arguments:
+            query (str) - the search query
+        Returns:
+            (list[SearchSection]) - search results, divided into sections
+        """
         search_component = SearchConcreteDecoratorTitle(SearchConcreteComponent())
         results = search_component.operation(query)
         return results
 
+
 class ConcreteSearchStrategyAuthor(SearchStrategy):
+    """
+    Strategy that searches for authors
+    """
+
     def search(self, query) -> list[SearchSection]:
+        """
+        Searches books database
+        Arguments:
+            query (str) - the search query
+        Returns:
+            (list[SearchSection]) - search results, divided into sections
+        """
         search_component = SearchConcreteDecoratorAuthor(SearchConcreteComponent())
         results = search_component.operation(query)
         return results

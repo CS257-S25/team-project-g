9b6238609caf070665b08b4dda456282d3359562
cteone <108945107+cteone@users.noreply.github.com>
2025-06-09 19:58:29 -0500

    updated map-book.js

--- static/map-book.js
+++ static/map-book.js
@@ -1,15 +1,15 @@
 /**
  * Module for creating a simple map plot for one book
  */
-import { BanMap, OneBookContent, DetailedRender} from "./map.js";
+import { BanMap, OneBookContent, DetailedRender } from "./map.js";
 
 window.addEventListener("DOMContentLoaded", async (_evt) => {
   const isbn = document.getElementsByName("isbn")[0].content
 
   const allContent = new OneBookContent(isbn)
-  const simpleRender = new SimpleRender()
+  const detailedRender = new DetailedRender()
 
-  const map = new BanMap(allContent, simpleRender)
+  const map = new BanMap(allContent, detailedRender)
   const plot = await map.createPlot()
 
   const div = document.querySelector("#map");
